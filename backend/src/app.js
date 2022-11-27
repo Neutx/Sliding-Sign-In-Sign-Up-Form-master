744ef0d8eacf35f0a299d45aee88ef6364dc0664
Neutx <dlprwz@gmail.com>
2022-11-27 19:34:26 +0530

    new commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
+const hbs = require('hbs');
+require("./db/conn");
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public")
+const template_path = path.join(__dirname, "../templates/views")
+const partials_path = path.join(__dirname, "../templates/partials")
+
+app.use(express.static(static_path))
+app.set('view engine', "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
 
 app.get("/", (req,res) => {
-    res.send("Hello from VIT Archive")
+    res.render("index")
 });
 
 app.listen(port, () => {
